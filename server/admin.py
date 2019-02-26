e96a27d238d56439843690e120dc53a2145c47f2
rshrc <rishieric91@gmail.com>
2019-02-26 18:50:52 +0530

    Clean Project without JetBrain Files

--- server/admin.py
+++ server/admin.py
@@ -1,10 +1,43 @@
 from django.contrib import admin
-from .models import Doctor, ConsultationTime, Medicine, Allergy, Child, Parent
-# Register your models here.
 
-admin.site.register(Doctor)
+from .models import Doctor, ConsultationTime, Medicine, Allergy, Child, Parent
+
+admin.site.site_header = "Allisto - We Do Good"
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'aadhar_number', 'specialization', 'email', 'phone_number')
+    list_filter = ('specialization', 'consultation_fee', 'working_hours')
+    search_fields = ('name', 'specialization', 'consultation_fee')
+
+
+@admin.register(Parent)
+class ParentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'aadhar_number', 'email', 'phone_number', 'address')
+    list_filter = ('name', 'email', 'phone_number')
+    search_fields = ('name', 'aadhar_number', 'email', 'phone_number', 'address')
+
+
+@admin.register(Child)
+class ChildAdmin(admin.ModelAdmin):
+    list_display = ('name', 'autistic', 'birthday', 'gender')
+    list_filter = ('name', 'autistic', 'birthday')
+    search_fields = ('name', 'autistic', 'birthday')
+
+
+@admin.register(Allergy)
+class AllergyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name', 'description')
+    search_fields = ('name',)
+
+
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name', 'description')
+    search_fields = ('name',)
+
+
 admin.site.register(ConsultationTime)
-admin.site.register(Medicine)
-admin.site.register(Allergy)
-admin.site.register(Child)
-admin.site.register(Parent)
