2102e19f0c182df5bbcc27687b655a0f23cf93a7
Rishi Banerjee <rishieric91@gmail.com>
2019-02-28 19:57:27 +0530

    Made the API's work

--- cryanalyze/models.py
+++ cryanalyze/models.py
@@ -5,9 +5,9 @@ class SmallData(models.Model):
     """
     dataset in use is dataset.xlsx
     """
-    parameter = models.DecimalField()
+    parameter = models.DecimalField(decimal_places=4, max_digits=10)
 
 
 class BigData(models.Model):
     """dataset in use is dataset-big.csv"""
-    parameter = models.DecimalField()
+    parameter = models.DecimalField(decimal_places=4, max_digits=10)
