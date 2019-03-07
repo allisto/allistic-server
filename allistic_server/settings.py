47748df8a729ac820dd50d6292ce1f4ad4e55662
Nedheesh Hasija <nedheesh.hasija@gmail.com>
2019-03-07 23:27:31 +0530

    STATIC_ROOT added to settings.py

--- allistic_server/settings.py
+++ allistic_server/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE.DIR, "static")
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
